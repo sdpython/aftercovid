4da1b42cf678d46ad914e0195a0eea485c237924
xavier dupré <xavier.dupre@gmail.com>
2021-03-24 09:44:51 +0100

    Fix failing unit test after data schema was updated on INSEE's site

--- tests/test_data_insee.py
+++ tests/test_data_insee.py
@@ -16,46 +16,31 @@ class TestDataInsee(unittest.TestCase):
     def test_data_covid_france_departments_hospitals(self):
         cache = "temp_hosp"
         df = data_covid_france_departments_hospitals(cache, metropole=True)
-        self.assertEqual(
-            list(
-                df.columns), [
-                'dep', 'sexe', 'jour', 'hosp', 'rea', 'rad', 'dc'])
+        exp_cols = ['dep', 'sexe', 'jour', 'hosp', 'rea', 'HospConv',
+                    'SSR_USLD', 'autres', 'rad', 'dc']
+        self.assertEqual(list(df.columns), exp_cols)
         df = data_covid_france_departments_hospitals(cache)
-        self.assertEqual(
-            list(
-                df.columns), [
-                'dep', 'sexe', 'jour', 'hosp', 'rea', 'rad', 'dc'])
+        self.assertEqual(list(df.columns), exp_cols)
 
     @skipif_appveyor("connectivity issue")
     @ignore_warnings(DtypeWarning)
     def test_data_covid_france_departments_tests(self):
         cache = "temp_tests"
         df = data_covid_france_departments_tests(cache, metropole=True)
-        self.assertEqual(
-            list(
-                df.columns), [
-                'dep', 'jour', 'P', 'T', 'cl_age90', 'pop'])
+        exp_cols = ['dep', 'jour', 'P', 'T', 'cl_age90', 'pop']
+        self.assertEqual(list(df.columns), exp_cols)
         df = data_covid_france_departments_tests(cache)
-        self.assertEqual(
-            list(
-                df.columns), [
-                'dep', 'jour', 'P', 'T', 'cl_age90', 'pop'])
+        self.assertEqual(list(df.columns), exp_cols)
 
     @skipif_appveyor("connectivity issue")
     def test_data_france_departments(self):
         cache = "temp_dep"
         df = data_france_departments(cache, metropole=True)
-        self.assertEqual(
-            list(sorted(df.columns)),
-            list(sorted(['code_depart', 'departement', 'code_region',
-                         'region', 'code_ancien', 'ancienne_re',
-                         'geometry'])))
+        exp_cols = ['code_depart', 'departement', 'code_region',
+                    'region', 'code_ancien', 'ancienne_re', 'geometry']
+        self.assertEqual(list(sorted(df.columns)), list(sorted(exp_cols)))
         df = data_france_departments(cache)
-        self.assertEqual(
-            list(sorted(df.columns)),
-            list(sorted(['code_depart', 'departement', 'code_region',
-                         'region', 'code_ancien', 'ancienne_re',
-                         'geometry'])))
+        self.assertEqual(list(sorted(df.columns)), list(sorted(exp_cols)))
 
 
 if __name__ == '__main__':
